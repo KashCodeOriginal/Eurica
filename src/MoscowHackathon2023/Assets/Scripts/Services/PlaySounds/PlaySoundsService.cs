3189a6d14a18c095c092fa086a4e3fb35910c0de
Kashfornoriginal <79724955+KashCodeOriginal@users.noreply.github.com>
2023-05-20 01:12:31 +0300

    Add sounds volume switch

--- src/MoscowHackathon2023/Assets/Scripts/Services/PlaySounds/PlaySoundsService.cs
+++ src/MoscowHackathon2023/Assets/Scripts/Services/PlaySounds/PlaySoundsService.cs
@@ -6,9 +6,17 @@ namespace Services.PlaySounds
     {
         private AudioSource _audioSource;
 
-        public void PlayOneShot(AudioClip audioClip, float volume)
+        public void PlayOneShot(AudioClip audioClip, VolumeLevel volume)
         {
-            _audioSource.PlayOneShot(audioClip);
+            var volumeLevel = volume switch
+            {
+                VolumeLevel.Default => 1f,
+                VolumeLevel.VoiceOver => 0.5f,
+                VolumeLevel.StepsVolume => 0.3f,
+                _ => 0f
+            };
+
+            _audioSource.PlayOneShot(audioClip, volumeLevel);
         }
 
         public void SetUp(AudioSource audioSource)
