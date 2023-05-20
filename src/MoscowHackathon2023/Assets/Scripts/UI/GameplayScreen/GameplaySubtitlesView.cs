36e57e4b31419e1410ef92b60b1a9def9ba108f3
Iaroslav Smirnov <elorenn@bk.ru>
2023-05-21 00:25:01 +0300

    Make small pause for 0.02 seconds between phrases

--- src/MoscowHackathon2023/Assets/Scripts/UI/GameplayScreen/GameplaySubtitlesView.cs
+++ src/MoscowHackathon2023/Assets/Scripts/UI/GameplayScreen/GameplaySubtitlesView.cs
@@ -1,3 +1,5 @@
+using Data.StaticData.VoicePhrases;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -9,15 +11,42 @@ namespace UI.GameplayScreen
 
         [SerializeField] private GameObject subtitlesViewParent;
         [SerializeField] private TextMeshProUGUI subtitlesOutput;
+        private VoiceMessage _voiceMessage;
 
         private void Awake()
         {
             RequestHidingSubtitles();
         }
 
-        public void RequestShowSubtitles()
+        public void RequestShowSubtitles(VoiceMessage voiceMessage)
         {
-            // TODO: Request entire class for subtitles and timings.
+            StopAllCoroutines();
+
+            _voiceMessage = voiceMessage;
+            StartCoroutine(PlaySubtitles());
+        }
+
+        private IEnumerator PlaySubtitles()
+        {
+            subtitlesViewParent.SetActive(true);
+
+            foreach (var phrase in _voiceMessage.Phrases)
+            {
+                if (phrase.Length <= 0.5f)
+                    Debug.LogError($"Phrase length is {phrase.Length} seconds, make it longer for ID {_voiceMessage.ID} in {phrase.Text}");
+
+                subtitlesOutput.text = phrase.Text;
+                
+                yield return new WaitForSeconds(phrase.Length - 0.2f);
+
+                subtitlesViewParent.SetActive(false);
+
+                yield return new WaitForSeconds(0.2f);
+
+                subtitlesViewParent.SetActive(true);
+            }
+
+            subtitlesViewParent.SetActive(false);
         }
 
         public void RequestHidingSubtitles()
