6bce5ffe7a1ea063f6ef2a57da6f94b457ec04d2
Iaroslav Smirnov <elorenn@bk.ru>
2023-05-20 14:16:23 +0300

    Get and save settings values, improve UI

--- src/MoscowHackathon2023/Assets/Scripts/UI/SettingsPanel/SettingsManager.cs
+++ src/MoscowHackathon2023/Assets/Scripts/UI/SettingsPanel/SettingsManager.cs
@@ -1,5 +1,9 @@
 using Services.Input;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 using Zenject;
 
 public class SettingsManager : MonoBehaviour
@@ -10,10 +14,19 @@ public class SettingsManager : MonoBehaviour
         _playerInputActionReader = playerInputActionReader;
     }
 
+    public UnityAction<bool> OnUpdatePanelState;
+    
+    [Header("Settings Panel")]
+    [SerializeField] private GameObject _settingsPanel;
     private PlayerInputActionReader _playerInputActionReader;
     private bool _isPanelOpened = false;
-    [SerializeField] private GameObject _settingsPanel;
 
+    [Header("Settings Data")]
+    [SerializeField] private Slider _volumeSlider;
+    [SerializeField] private TextMeshProUGUI _volumeOutput;
+    [SerializeField] private Slider _mouseSensSlider;
+    [SerializeField] private TextMeshProUGUI _mouseSensOutput;
+    
     private void Awake()
     {
         _settingsPanel.SetActive(false);
@@ -22,6 +35,9 @@ public class SettingsManager : MonoBehaviour
     private void OnEnable()
     {
         _playerInputActionReader.IsPlayerEscClicked += EscClicked;
+
+        _volumeSlider.value = PlayerPrefs.GetFloat("GameVolume", 0.5f);
+        _mouseSensSlider.value = PlayerPrefs.GetFloat("MouseSensivity", 8f);
     }
 
     private void OnDisable()
@@ -32,6 +48,29 @@ public class SettingsManager : MonoBehaviour
     private void EscClicked()
     {
         _isPanelOpened = !_isPanelOpened;
+        UpdatePanelState();
+    }
+
+    public void ClosePanel()
+    {
+        _isPanelOpened = false;
+        UpdatePanelState();
+    }
+
+    private void Update()
+    {
+        PlayerPrefs.SetFloat("GameVolume", _volumeSlider.value);
+        PlayerPrefs.SetFloat("MouseSensivity", _mouseSensSlider.value);
+
+        _volumeOutput.text = Mathf.RoundToInt(_volumeSlider.value * 100) + "%";
+        _mouseSensOutput.text = _mouseSensSlider.value.ToString("0.0");
+
+        // TODO: Invoke OnSettingsData updated.
+    }
+
+    private void UpdatePanelState()
+    {
         _settingsPanel.SetActive(_isPanelOpened);
+        OnUpdatePanelState?.Invoke(_isPanelOpened);
     }
 }
