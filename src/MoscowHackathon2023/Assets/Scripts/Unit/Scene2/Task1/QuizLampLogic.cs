3dc1022f5265b22380e60f25c1221dfdbf5e831d
Iaroslav Smirnov <elorenn@bk.ru>
2023-05-22 01:20:01 +0300

    Finish making lamp lever quiz

---
+++ src/MoscowHackathon2023/Assets/Scripts/Unit/Scene2/Task1/QuizLampLogic.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Unit.Scene2.Task1
+{
+    public class QuizLampLogic : MonoBehaviour
+    {
+        public UnityAction OnQuizCompleted;
+        
+        private int[] _progress;
+        [SerializeField] private MeshRenderer[] _indicators;
+        [SerializeField] private Material _indicatorOn;
+        [SerializeField] private Material _indicatorOff;
+
+        private void Awake()
+        {
+            _progress = new int[5];
+            foreach (var indicator in _indicators)
+            {
+                indicator.material = _indicatorOff;
+            }
+        }
+
+        /// <param name="changes">0 set red, 1 set green, -1 ignore</param>
+        public void SetChanges(int[] changes)
+        {
+            for (int i = 0; i < changes.Length; i++)
+            {
+                if (changes[i] != -1)
+                {
+                    _progress[i] = changes[i];
+                    _indicators[i].material = _progress[i] == 0 ? _indicatorOff : _indicatorOn;
+                }
+            }
+
+            if (IsCompleted())
+            {
+                OnQuizCompleted?.Invoke();
+            }
+        }
+
+        private bool IsCompleted()
+        {
+            bool allGreen = true;
+
+            foreach (var progress in _progress)
+            {
+                if (progress == 0)
+                {
+                    allGreen = false;
+                }
+            }
+            return allGreen;
+        }
+    }
+}
