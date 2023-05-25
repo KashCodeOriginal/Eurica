7b28eb60f4479cc5b85acf0ae34893f19bc6a120
Iaroslav Smirnov <elorenn@bk.ru>
2023-05-26 01:36:55 +0300

    Configure weapon sway effect

--- src/MoscowHackathon2023/Assets/Scripts/Unit/UniversalGun/WeaponSway.cs
+++ src/MoscowHackathon2023/Assets/Scripts/Unit/UniversalGun/WeaponSway.cs
@@ -5,7 +5,6 @@ namespace Unit.UniversalGun
     public class WeaponSway : MonoBehaviour
     {
         [SerializeField] private float _swayAmount = 0.02f;
-        [SerializeField] private float _maxSwayAmount = 0.05f;
         [SerializeField] private float _swaySmoothness = 4.0f; 
 
         private Quaternion initialRotation;
@@ -17,7 +16,7 @@ namespace Unit.UniversalGun
         {
             weaponTransform = transform;
             initialRotation = weaponTransform.localRotation;
-            cameraTransform = transform.parent;
+            cameraTransform = transform.parent.parent;
             lastCameraPosition = cameraTransform.position;
         }
 
@@ -27,9 +26,6 @@ namespace Unit.UniversalGun
 
             float swayX = -cameraDelta.x * _swayAmount;
             float swayY = -cameraDelta.y * _swayAmount;
-
-            swayX = Mathf.Clamp(swayX, -_maxSwayAmount, _maxSwayAmount);
-            swayY = Mathf.Clamp(swayY, -_maxSwayAmount, _maxSwayAmount);
 
             Quaternion targetRotation = Quaternion.Euler(swayY, swayX, 0f) * initialRotation;
             weaponTransform.localRotation = Quaternion.Slerp(weaponTransform.localRotation, targetRotation, Time.deltaTime * _swaySmoothness);
