e8ea0914df9661dfcd71d0389b15692d8a739e47
Iaroslav Smirnov <elorenn@bk.ru>
2023-05-25 11:10:09 +0300

    Minor fixes in Scene1 and GameplaySettings

--- src/MoscowHackathon2023/Assets/Scripts/Data/StaticData/PlayerData/GameplaySettings.cs
+++ src/MoscowHackathon2023/Assets/Scripts/Data/StaticData/PlayerData/GameplaySettings.cs
@@ -5,15 +5,8 @@ namespace Data.StaticData.PlayerData
     [CreateAssetMenu(menuName = "StaticData/Game/GameplaySettings", fileName = "GameplaySettings")]
     public class GameplaySettings : ScriptableObject
     {
-#if UNITY_EDITOR
         [field: SerializeField] public float SoundVolume = 0.3f;
         [field: SerializeField] public float MouseSens = 15f;
         [field: SerializeField] public bool Subtitles = true;
-#else
-        // Use different values for release build
-        public float SoundVolume = 0.5f;
-        public float MouseSens = 15f;
-        public bool Subtitles = true;
-#endif
     }
 }
