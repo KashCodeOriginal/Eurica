aa317e4a3b072ea125efe7279fd0aaacb39e108d
Kashfornoriginal <79724955+KashCodeOriginal@users.noreply.github.com>
2023-05-28 00:01:41 +0300

    Add sound helper, add base fifth scene design

---
+++ src/MoscowHackathon2023/Assets/Scripts/Unit/TriggerSystem/SoundHelper.cs
@@ -0,0 +1,31 @@
+using Services.PlaySounds;
+using UnityEngine;
+using Zenject;
+
+namespace Unit.TriggerSystem
+{
+    public class SoundHelper : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _audioClip;
+        [SerializeField] private bool _canPlayMultiple;
+        [SerializeField] private bool _playOnlyOnce;
+
+        private IPlaySoundsService _playSoundsService;
+
+        [Inject]
+        public void Construct(IPlaySoundsService playSoundsService)
+        {
+            _playSoundsService = playSoundsService;
+        }
+
+        public void PlaySound(float afterTime = 0f)
+        {
+            Invoke(nameof(Play), afterTime);
+        }
+
+        private void Play()
+        {
+            _playSoundsService.PlayAudioClip(_audioClip, VolumeLevel.Default, _canPlayMultiple, _playOnlyOnce);
+        }
+    }
+}
