b9d2ff1235201f84b8bce5dc23a3c1f7c0fdf26d
Iaroslav Smirnov <elorenn@bk.ru>
2023-05-28 22:22:25 +0300

    Add exit button

--- src/MoscowHackathon2023/Assets/Scripts/UI/MainMenuScreen/MainMenuScreenController.cs
+++ src/MoscowHackathon2023/Assets/Scripts/UI/MainMenuScreen/MainMenuScreenController.cs
@@ -9,17 +9,25 @@ namespace UI.MainMenuScreen
     public class MainMenuScreenController : MonoBehaviour
     {
         [SerializeField] private Button _startGameButton;
+        [SerializeField] private Button _exitGame;
 
         public event Action OnPlayButtonClicked;
 
         private void Start()
         {
             _startGameButton.onClick.AddListener(PlayButtonPressed);
+            _exitGame.onClick.AddListener(ExitButtonPressed);
         }
 
         private void PlayButtonPressed()
         {
             StartCoroutine(PlayButtonPressedDelay());
+        }
+
+        private void ExitButtonPressed()
+        {
+            Debug.Log("Exit app");
+            Application.Quit();
         }
 
         private IEnumerator PlayButtonPressedDelay()
@@ -36,6 +44,7 @@ namespace UI.MainMenuScreen
         private void OnDisable()
         {
             _startGameButton.onClick.RemoveListener(PlayButtonPressed);
+            _exitGame.onClick.RemoveListener(ExitButtonPressed);
         }
     }
 }
