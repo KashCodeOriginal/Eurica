d005bcaedcf4a5ecdcaf9a846503b80e9a7b0f9e
Iaroslav Smirnov <elorenn@bk.ru>
2023-05-28 04:08:54 +0300

    Create SoundsSceneSource component for in-scene audio

---
+++ src/MoscowHackathon2023/Assets/Scripts/Services/PlaySounds/SoundsSceneSource.cs
@@ -0,0 +1,32 @@
+﻿using UI.GameplayScreen;
+using UnityEngine;
+
+namespace Services.PlaySounds
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundsSceneSource : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+        private float _startVolume;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _startVolume = _audioSource.volume;
+
+            if (GameplayScreen.Instance)
+            {
+                UpdateCurrentVolume(GameplayScreen.Instance.SettingsManager.GetVolume());
+            }
+            else
+            {
+                UpdateCurrentVolume(0f);
+            }
+        }
+
+        public void UpdateCurrentVolume(float volume)
+        {
+            _audioSource.volume = _startVolume * volume;
+        }
+    }
+}
